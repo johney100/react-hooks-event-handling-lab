7ef284e4e8147485904bb5a11bddbbd8c3a99d5f
johney <johneyburke@gmail.com>
2024-01-16 15:51:32 -0700

    tests passed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+import React from "react";
+import ReactDOM from 'react-dom';
+
+function EyesOnMe(){
+
+    function handleFocus(){
+        console.log("Good!");
+    }
+
+    function handleBlur(){
+        console.log("Hey! Eyes on me!");
+    }
+
+    return(
+        <div>
+            <button  text="Eyes on me" onFocus={handleFocus} onBlur={handleBlur}>Eyes on me </button>
+        </div>
+
+    )
+
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,23 @@
 // Code Keypad Component Here
+import React from "react";
+import EyesOnMe from "./EyesOnMe";
 
 function Keypad (){
+
+        function handleChange(event){
+            console.log("Entering password...");
+
+        }
     return (
-        <div></div>
+        <div>
+            <input 
+            type="password"
+            placeholder="Enter password"
+            onChange={handleChange}
+
+            />
+
+        </div>
     )
 }
 
